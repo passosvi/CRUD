7ebd20959d5cd65506510e19fdd523779303ccb7
Victor <vitor.passos@gmail.com>
2025-02-08 00:29:38 -0300

    Warning about schema

---
+++ backend/schemas.py
@@ -0,0 +1,31 @@
+#FUNÇAO DE VIEW, CADA OPERAÇAO TEM UM PADRAO DISTINTO QUE PRECISA SER VALIDADO
+from pydantic import BaseModel,PositiveFloat,EmailStr,validators
+from enum import Enum
+from datetime import datetime
+from typing import Optional
+
+class ProductBase(BaseModel):
+    name = str
+    description = str
+    price = PositiveFloat
+    category = str
+    email_supplier = EmailStr
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class ProductReponse(ProductBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class ProductUpdate(ProductBase):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[PositiveFloat] = None
+    category: Optional[str] = None
+    email_supplier: Optional[EmailStr] = None
+    
